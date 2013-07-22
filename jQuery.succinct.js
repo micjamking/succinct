1ed8ccf4f147e34db049d4765b06df38e5519e2b
Mike King <mike@85pixels.com>
2013-07-22 13:15:08 -1000

    Create jQuery.succint.js & built plugin structure
    Added copyright, jQuery plugin structure (including options)

---
+++ jQuery.succinct.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2013 Mike King (@micjamking)
+ * 
+ * jQuery Succinct plugin
+ * Version 1.0 (July 2013)
+ *
+ * Licensed under the MIT License
+ */
+ 
+ (function(){
+ 	$.fn.succinct = function(options){
+ 		
+ 		var defaults = {};
+ 		
+ 		var options = $.extend(defaults, options);
+ 		
+ 		return this.each(function(){
+ 		
+ 		});
+ 	};
+ })(jQuery);
