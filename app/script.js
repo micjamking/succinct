2334825342eb8b704717147a9b7f8aa5ef612b00
Mike King <mike@85pixels.com>
2014-10-04 20:45:14 -1000

    Removing color setting from buttons to prevent flicker

--- app/script.js
+++ app/script.js
@@ -27,7 +27,7 @@
 
 			colorful = function() {
 				var hue = Math.floor(Math.random()*359),
-					styles = '<style>.color {background-color: hsl(' + hue + ', 36%, 48%);color: hsl(' + hue + ', 36%, 48%);border-color: hsl(' + hue + ', 36%, 48%);}.color-light {border-color: hsla(' + hue + ', 100%, 48%,0.75);}.link:hover > span,.link:focus > span,.link:active > span,.link > span:after {background: hsl(' + hue + ', 36%, 48%);}input[type="submit"]:hover,input[type="submit"]:focus,.button{background-color: hsl(' + hue + ', 36%, 43%);}input[type="text"]:focus{box-shadow: 0 0 5px hsl(' + hue + ', 54%, 50%);}</style>';
+					styles = '<style>.color {background-color: hsl(' + hue + ', 36%, 48%);color: hsl(' + hue + ', 36%, 48%);border-color: hsl(' + hue + ', 36%, 48%);}.color-light {border-color: hsla(' + hue + ', 100%, 48%,0.75);}.link:hover > span,.link:focus > span,.link:active > span,.link > span:after {background: hsl(' + hue + ', 36%, 48%);}input[type="submit"]:hover,input[type="submit"]:focus{background-color: hsl(' + hue + ', 36%, 43%);}input[type="text"]:focus{box-shadow: 0 0 5px hsl(' + hue + ', 54%, 50%);}</style>';
 
 				$('head').append($(styles));
 			};
